daacd2510620cb2847e9689981a2492df7b9ee81
akegermany <mail@akesting.de>
2019-03-20 15:13:44 +0100

    cleanup

--- src/main/java/de/akesting/AdaptiveSmoothingMethodMain.java
+++ src/main/java/de/akesting/AdaptiveSmoothingMethodMain.java
@@ -35,18 +35,13 @@ import de.akesting.xml.XmlInputLoader;
 
 public class AdaptiveSmoothingMethodMain {
 
-	public static final String[][] LANETYPE_GROUPS = {{ "ML" }, { "HOV" }};
-	//public static final String[][] LANETYPE_GROUPS = {{ "ML" }, { "HOV" }, { "ML", "HOV" }};
-	public static final String FILE_DATEFORMAT= "yyyy_MM_dd";
-	
-	public Long loadTimeTotal = new Long(0);
-	public Long smoothWriteBackTimeTotal = new Long(0);
-	
-	
-	public static void main(String[] args) {
+    private Long loadTimeTotal = new Long(0);
+    private Long smoothWriteBackTimeTotal = new Long(0);
+
+    public static void main(String[] args) {
         new AdaptiveSmoothingMethodMain().run(args);
     }
-    
+
     public void run(String[] args) {
         // Set the default locale
         Locale.setDefault(Locale.US);
@@ -58,108 +53,110 @@ public class AdaptiveSmoothingMethodMain {
 
         // handling two different input formats
         if (inputData.isSetInputCalifornia()) {
-           	processCaliforniaData(cmdLine, inputData);            	
+            processCaliforniaData(cmdLine, inputData);
         } else {
             DataRepository dataRep = new DataRepository(cmdLine.defaultReposFilename(),
                     cmdLine.defaultFilteredDataFilename(), inputData.getInput(), cmdLine.absolutePath());
-            // TODO Intended? Might break! [SM]
             applyAsm(cmdLine, inputData, dataRep);
-        }        
+        }
         System.out.println("done.");
     }
 
     private void processCaliforniaData(final ReadCommandline cmdLine, final AdaptiveSmoothingMethodProject inputData) {
+        final String fileDateformat = "yyyy_MM_dd";
+        final String[][] lanetypeGroups = {{"ML"}, {"HOV"}};
+
         InputCalifornia inputCalifornia = inputData.getInputCalifornia();
         CaliforniaInfrastructure californiaInfrastructure = new CaliforniaInfrastructure(inputCalifornia);
-        final CaliforniaDataReader reader =  new CaliforniaDataReader(inputCalifornia);
-    	
+        final CaliforniaDataReader reader = new CaliforniaDataReader(inputCalifornia);
+
         // Initialize thread pool
         ExecutorService pool = Executors.newFixedThreadPool(cmdLine.getNumThreads());
-    	
+
         // Get pairs of freeway and stretches for fast lookup
-        final Map<Freeway, FreewayStretch> freewayStretches = Collections.synchronizedMap(new HashMap<Freeway, FreewayStretch>());        
-        for(Freeway freeway : inputCalifornia.getFreeways().getFreeway()){
-        	freewayStretches.put(freeway, californiaInfrastructure.getFreewayStretch(freeway.getName()));
+        final Map<Freeway, FreewayStretch> freewayStretches = Collections.synchronizedMap(new HashMap<Freeway, FreewayStretch>());
+        for (Freeway freeway : inputCalifornia.getFreeways().getFreeway()) {
+            freewayStretches.put(freeway, californiaInfrastructure.getFreewayStretch(freeway.getName()));
         }
         // Get list of all districts for selected freeways (positive filter)
-		final Set<String> allDistricts = new HashSet<String>();
-		for (FreewayStretch fs : freewayStretches.values()) allDistricts.addAll(Lists.newArrayList(fs.getDistricts()));
-		
-		long startTimeOverall = System.currentTimeMillis();
-    	// iterate over each day within interval  
+        final Set<String> allDistricts = new HashSet<String>();
+        for (FreewayStretch fs : freewayStretches.values()) allDistricts.addAll(Lists.newArrayList(fs.getDistricts()));
+
+        long startTimeOverall = System.currentTimeMillis();
+        // iterate over each day within interval
         for (DateTime day = reader.getfromTime(); day.isBefore(reader.gettoTime()); day = day.plusDays(1)) {
-        	final String datetime = day.toString(DateTimeFormat.forPattern(FILE_DATEFORMAT));
-        	// Start/Enqueue new thread for given day
-			// TODO Consider different thread types for division of labor (read vs asm) [SM]
-        	pool.execute(new Runnable() {
-				public void run() {
-					long startTime = System.currentTimeMillis();
-					// Get data repositories for each freeway and lane group (list of lists)
-					// Only read files once for all freeways at a time
-        			HashMap<Freeway, HashMap<String[], DataRepository>> dataRepos = reader.loadRepos(freewayStretches, datetime, LANETYPE_GROUPS, allDistricts);
-        			long loadTime = System.currentTimeMillis();
-        			// iterate over each freeway...
-					for (Entry<Freeway, HashMap<String[], DataRepository>> freewayRepos : dataRepos.entrySet()) {
-						// Construct file name along the way
-						String freewayName = freewayRepos.getKey().getName();
-						File freewayDir = new File(cmdLine.absolutePath(), freewayName);
-						// Create freeway sub directory (synchronized)
-						if (!AdaptiveSmoothingMethodMain.createDir(freewayDir)) {
-							System.err.println("Error creating dir: "+freewayName);
-							continue;
-						}
-						// ...and each lane group for this freeway
-						for (Entry<String[], DataRepository>  laneGroupRepo : freewayRepos.getValue().entrySet()) {
-							DataRepository dataRepository = laneGroupRepo.getValue();
-							String laneGroupName = "";
-							for (String lane : laneGroupRepo.getKey()) laneGroupName += lane;
-							// Write back data repository
-							// TODO Is this really necessary? [SM]
-							File repoOutputFile = new File(freewayDir, datetime + "-loadedData-" + freewayName + "-" + laneGroupName + ".dat");
-							if (!(repoOutputFile).exists()) {
-								dataRepository.writeRepository(repoOutputFile);
-							} else {
-								System.out.println("Skip existing data repository: "+repoOutputFile.getParentFile().getName()+File.separator+repoOutputFile.getName());
-							}
-							// Do adaptive smoothing
-							File asmOutputFile = new File(freewayDir, datetime + "-" + freewayName + "-" + laneGroupName + ".GASM");
-							if (!(asmOutputFile).exists()) {
-								applyAsm(cmdLine, inputData, dataRepository, asmOutputFile.getAbsolutePath());
-							} else {
-								System.out.println("Skip existing ASM: "+asmOutputFile.getParentFile().getName()+File.separator+asmOutputFile.getName());
-							}							
-						}
-					}					
-					long endTime = System.currentTimeMillis();
-					// Performance measurement
-					synchronized(loadTimeTotal) {
-						loadTimeTotal += (loadTime - startTime); 
-					}
-					synchronized(smoothWriteBackTimeTotal) {
-						smoothWriteBackTimeTotal += (endTime - loadTime);
-					}					
-				}
-        	});
+            final String datetime = day.toString(DateTimeFormat.forPattern(fileDateformat));
+            // Start/Enqueue new thread for given day
+            // TODO Consider different thread types for division of labor (read vs asm) [SM]
+            pool.execute(new Runnable() {
+                public void run() {
+                    long startTime = System.currentTimeMillis();
+                    // Get data repositories for each freeway and lane group (list of lists)
+                    // Only read files once for all freeways at a time
+                    HashMap<Freeway, HashMap<String[], DataRepository>> dataRepos = reader.loadRepos(freewayStretches, datetime, lanetypeGroups, allDistricts);
+                    long loadTime = System.currentTimeMillis();
+                    // iterate over each freeway...
+                    for (Entry<Freeway, HashMap<String[], DataRepository>> freewayRepos : dataRepos.entrySet()) {
+                        // Construct file name along the way
+                        String freewayName = freewayRepos.getKey().getName();
+                        File freewayDir = new File(cmdLine.absolutePath(), freewayName);
+                        // Create freeway sub directory (synchronized)
+                        if (!AdaptiveSmoothingMethodMain.createDir(freewayDir)) {
+                            System.err.println("Error creating dir: " + freewayName);
+                            continue;
+                        }
+                        // ...and each lane group for this freeway
+                        for (Entry<String[], DataRepository> laneGroupRepo : freewayRepos.getValue().entrySet()) {
+                            DataRepository dataRepository = laneGroupRepo.getValue();
+                            String laneGroupName = "";
+                            for (String lane : laneGroupRepo.getKey()) laneGroupName += lane;
+                            // Write back data repository
+                            // TODO Is this really necessary? [SM]
+                            File repoOutputFile = new File(freewayDir, datetime + "-loadedData-" + freewayName + "-" + laneGroupName + ".dat");
+                            if (!(repoOutputFile).exists()) {
+                                dataRepository.writeRepository(repoOutputFile);
+                            } else {
+                                System.out.println("Skip existing data repository: " + repoOutputFile.getParentFile().getName() + File.separator + repoOutputFile.getName());
+                            }
+                            // Do adaptive smoothing
+                            File asmOutputFile = new File(freewayDir, datetime + "-" + freewayName + "-" + laneGroupName + ".GASM");
+                            if (!(asmOutputFile).exists()) {
+                                applyAsm(cmdLine, inputData, dataRepository, asmOutputFile.getAbsolutePath());
+                            } else {
+                                System.out.println("Skip existing ASM: " + asmOutputFile.getParentFile().getName() + File.separator + asmOutputFile.getName());
+                            }
+                        }
+                    }
+                    long endTime = System.currentTimeMillis();
+                    // Performance measurement
+                    synchronized (loadTimeTotal) {
+                        loadTimeTotal += (loadTime - startTime);
+                    }
+                    synchronized (smoothWriteBackTimeTotal) {
+                        smoothWriteBackTimeTotal += (endTime - loadTime);
+                    }
+                }
+            });
         }
-    	pool.shutdown();
-    	try {
-    		// Basically wait for all threads to complete their work
-    		pool.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
-    	} catch (InterruptedException e) {
-    		System.exit(1);
-    	}
-    	long endTimeOverall = System.currentTimeMillis();
-    	System.out.println("run time: " + (endTimeOverall-startTimeOverall)/1000 + "s");
-    	// Return aggregated thread time (!= run time).
-        System.out.println("threads: "+cmdLine.getNumThreads()+" | loadTimeTotal: "+(loadTimeTotal / 1000)+"s  | smoothWriteBackTimeTotal: "+(smoothWriteBackTimeTotal / 1000)+"s");    	
-    }
-    
-    // Apply ASM with defaultOutputName
-    private void applyAsm(ReadCommandline cmdLine, AdaptiveSmoothingMethodProject inputData, DataRepository dataRep) {
-    	applyAsm(cmdLine, inputData, dataRep, cmdLine.defaultOutFilename());
+        pool.shutdown();
+        try {
+            // Basically wait for all threads to complete their work
+            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.exit(1);
+        }
+        long endTimeOverall = System.currentTimeMillis();
+        System.out.println("run time: " + (endTimeOverall - startTimeOverall) / 1000 + "s");
+        // Return aggregated thread time (!= run time).
+        System.out.println("threads: " + cmdLine.getNumThreads() + " | loadTimeTotal: " + (loadTimeTotal / 1000) + "s  | smoothWriteBackTimeTotal: " + (smoothWriteBackTimeTotal / 1000) + "s");
     }
 
-	private void applyAsm(ReadCommandline cmdLine, AdaptiveSmoothingMethodProject inputData, DataRepository dataRep, String outFileName) {
+    // Apply ASM with defaultOutputName
+    private void applyAsm(ReadCommandline cmdLine, AdaptiveSmoothingMethodProject inputData, DataRepository dataRep) {
+        applyAsm(cmdLine, inputData, dataRep, cmdLine.defaultOutFilename());
+    }
+
+    private void applyAsm(ReadCommandline cmdLine, AdaptiveSmoothingMethodProject inputData, DataRepository dataRep, String outFileName) {
         DataView dataView = new DataView(inputData.getVirtualGrid(), dataRep);
 
         // TODO handling of many California freeways in batch
@@ -196,15 +193,15 @@ public class AdaptiveSmoothingMethodMain {
                     .getLocationSeriesOutput(), outputGrid);
         }
     }
-	
-	public static synchronized boolean createDir(File dir) {
-		try {
-			if (!dir.exists()) dir.mkdir();
-			return dir.isDirectory();			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+
+    private static boolean createDir(File dir) {
+        try {
+            if (!dir.exists()) dir.mkdir();
+            return dir.isDirectory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }
